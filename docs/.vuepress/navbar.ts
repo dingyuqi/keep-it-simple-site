48608ac9aa61bebd4a95390049292c44e6280690
dingyuqi <dingyq2023@gmail.com>
2024-11-13 18:18:37 +0800

    theme: update navbar

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -1,17 +1,17 @@
 import { defineNavbarConfig } from 'vuepress-theme-plume'
 
 export const zhNavbar = defineNavbarConfig([
-  { text: '首页', link: '/', icon: 'fluent-color:home-48' },
-  { text: '博客', link: '/blog/', icon: 'fxemoji:pages' },
+  { text: '首页', link: '/', icon: 'flat-color-icons:home' },
+  { text: '博客', link: '/blog/', icon: 'flat-color-icons:view-details' },
   {
     text: '系列文章',
-    items: [      
-      { text: '数据挖掘', link: '/dataMining/', icon: 'material-symbols:database-outline' },
-      { text: '论文笔记', link: '/paperNote/', icon: 'material-symbols:note' },
-      { text: '面向对象', link: '/designModel/', icon: 'material-symbols:code-blocks' },
-      { text: '面试题目', link: '/interview/', icon: 'clarity:employee-line' },
-    ],
-    icon: 'fxemoji:books'
+    icon: 'flat-color-icons:folder',
+    items: [
+      { text: '数据挖掘', link: '/dataMining/', icon: 'flat-color-icons:combo-chart' },
+      { text: '论文笔记', link: '/paperNote/', icon: 'flat-color-icons:library' },
+      { text: '编程设计', link: '/designModel/', icon: 'flat-color-icons:command-line' },
+      { text: '面试题目', link: '/interview/', icon: 'flat-color-icons:faq' },
+    ]
   },
 ])
 
