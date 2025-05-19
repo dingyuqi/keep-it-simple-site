9d0ddcdce58b1a406bc0442b2a4620eca21824e1
dingyuqi <dingyq2023@gmail.com>
2025-05-19 09:16:12 +0800

    feature(theme): remove .vp-doc table

--- docs/.vuepress/theme/shim.d.ts
+++ docs/.vuepress/theme/shim.d.ts
@@ -1,6 +1,6 @@
 declare module '*.vue' {
-  import type { ComponentOptions } from 'vue'
+    import type { ComponentOptions } from 'vue'
 
-  const comp: ComponentOptions
-  export default comp
+    const comp: ComponentOptions
+    export default comp
 }
